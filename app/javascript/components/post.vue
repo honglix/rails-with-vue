b1614a3f954950b45b8e602be9666656ae7fca02
Hongli Xu <wu402a8zc7@gmail.com>
2020-04-18 15:35:37 -0400

    add element-ui

--- app/javascript/components/post.vue
+++ app/javascript/components/post.vue
@@ -3,11 +3,21 @@
     <h2>{{ post.title }}</h2>
     <p>{{ post.body }}</p>
     <hr />
+
+      <el-radio v-model="radio" label="1">备选项</el-radio>
+  <el-radio v-model="radio" label="2">备选项</el-radio>
+
+
   </div>
 </template>
 
 <script>
 export default {
-  props: ['post']
+  props: ['post'],
+    data () {
+      return {
+        radio: '1'
+      };
+    }
 }
 </script>
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,10 @@ import router from '../routes.js';
 
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 Vue.use(VueAxios, axios)
 
 const app = new Vue({
