64fced2b811caa5907dcc40cdcf717e614fe5417
Hongli Xu <wu402a8zc7@gmail.com>
2020-04-20 00:29:12 -0400

    retrive user list

--- app/javascript/pages/home/index.vue
+++ app/javascript/pages/home/index.vue
@@ -4,6 +4,15 @@
       <el-main></el-main>
     </el-container>
 
+    <el-dialog title="讓我知道你叫什麼？" :visible.sync="dialogVisible" width="30%">
+      <span>Nickname:</span>
+      <el-input v-model="nickname" placeholder="请输入"></el-input>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false; createUser()">确 定</el-button>
+      </span>
+    </el-dialog>
+
     <el-row :gutter="24" justify="center" text-align="center">
       <el-col :span="4">
         <div class="grid-content bg-purple">.</div>
@@ -22,18 +31,25 @@
             <el-button style="margin-top: 12px;" @click="next">下一步</el-button>
             <el-button v-on:click="getPosts" v-if="active == 1">點我看笑話</el-button>
             <el-container>
-              <el-main v-if = "active != 0 ">{{posts}}</el-main>
+              <el-main v-if="active != 0 ">{{posts}}</el-main>
             </el-container>
 
             <el-container>
               <div class="grid-content bg-purple">
                 <div class="block" v-if="active == 3 ">
-                  <span class="demonstration">給個評分吧</span>
+                  <span class="demonstration">給個評分吧!</span>
                   <el-rate v-model="value1"></el-rate>
                 </div>
               </div>
             </el-container>
-          </el-main>
+          </el-main>誰用了這個app?
+          <el-timeline :reverse="reverse">
+            <el-timeline-item
+              v-for="(user, index) in allUser"
+              :key="index"
+              :timestamp="user.created_at"
+            >{{user.nickname}}</el-timeline-item>
+          </el-timeline>
         </el-container>
       </el-col>
       <el-col :span="4"></el-col>
@@ -52,10 +68,21 @@ export default {
     return {
       posts: null,
       active: 0,
-      value1: null
+      value1: null,
+      dialogVisible: false,
+      nickname: "",
+      allUser: [],
+      reverse: true
     };
   },
+  created() {
+    this.axios.get("/api/v1/users").then(response => {
+      this.allUser = response.data;
+    });
+  },
+
   methods: {
+    getUserList: function() {},
     getPosts: function() {
       this.axios
         .get("https://autumnfish.cn/api/joke/list?&num=1")
@@ -65,9 +92,22 @@ export default {
     },
     next() {
       if (this.active++ > 2) this.active = 0;
-      if(this.active == 1){
-        this.posts = ""
+      if (this.active == 1) {
+        (this.posts = ""), (this.dialogVisible = true);
       }
+    },
+    createUser() {
+      console.log("Heel");
+      this.axios
+        .post("/api/v1/users", {
+          nickname: this.nickname
+        })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     }
   }
 };
